11a472295f4791b6562106612f24cbeed731cc49
Subham Rakshit <developersubham@gmail.com>
2017-03-10 23:39:57 +0000

    Parameterised zookeeper connection string

--- kafka-properties.sh
+++ kafka-properties.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #set kafka server properties
-sed -i -e "s|^zookeeper.connect=.*$|zookeeper.connect=zookeeper:2181|" $KAFKA_HOME/config/server.properties
+sed -i -e "s|^zookeeper.connect=.*$|zookeeper.connect=$ZK_CONNECT_STRING|" $KAFKA_HOME/config/server.properties
 sed -i -e "s|^#delete.topic.enable=.*$|delete.topic.enable=true|" $KAFKA_HOME/config/server.properties
 sed -i -e "s|^broker.id=.*$|broker.id=-1|" $KAFKA_HOME/config/server.properties
